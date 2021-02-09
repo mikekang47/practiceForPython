fef17dc17e038a9966d1480c08cf52b6275fbd43
mikekang47 <gyeong0308@gmail.com>
2021-02-09 15:14:13 +0900

    Solve quiz4 about list and shuffle and sample

---
+++ quiz-4.py
@@ -0,0 +1,9 @@
+from random import *
+
+users = list(range(1, 21))
+shuffle(users)
+winners = sample(users,4)
+print(" -- 당첨자 발표 -- ")
+print("치킨 당첨자 : {0}".format(winners[0]))
+print("커피장첨자 : {0}".format(winners[1:]))
+print(" -- 축하합니다 -- ")
