ac65e1656f7525115c60b0611e1e3ac8cec4ff56
mikekang47 <gyeong0308@gmail.com>
2021-02-18 17:26:11 +0900

    Make a character icon

--- pygame_basic/3_main_sprite.py
+++ pygame_basic/3_main_sprite.py
@@ -13,7 +13,14 @@ pygame.display.set_caption("Demo game")
 
 background = pygame.image.load("/Users/mikekang/work/practiceForPython/pygame_basic/background.png")
 
-character = pygame.image.load()
+character = pygame.image.load("/Users/mikekang/work/practiceForPython/pygame_basic/character.jpg")
+character_size = character.get_rect().size #이미지 크기 구해옴
+character_width = character_size[0] #캐릭터 가로크기
+character_height = character_size[1] #캐릭터 세로 크기
+character_x_pos = (screen_width  / 2) - (character_width / 2) #화면 가로 절반 크기에 해당하는 곳에 위치
+character_y_pos = screen_height - character_height
+
+
 
 running = True
 while running:
@@ -21,6 +28,9 @@ while running:
        if event.type == pygame.QUIT: #창이 닫히는 이벤트가 발생하면
            running = False #게임이 끝남
    screen.blit(background, (0,0))
+
+   screen.blit(character, (character_x_pos, character_y_pos))
+
    pygame.display.update()
 
 
